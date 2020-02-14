b1639e66b2c4c218239c0d40e3acd23c3f1d1ca4
Gustavo Vidal <reis.vidal21@gmail.com>
2020-02-14 16:57:59 -0300

    bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,11 +14,11 @@ public class Program {
 		 Scanner sc = new Scanner(System.in);
 	     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	     
-	     System.out.print("Room number:");
+	     System.out.print("Room number: ");
 	     int number = sc.nextInt();
-	     System.out.println("Check-in date (dd/MM/yyyy): ");
+	     System.out.print("Check-in date (dd/MM/yyyy): ");
 	     Date checkIn = sdf.parse(sc.next());
-	     System.out.println("Check-out date (dd/MM/yyyy): ");
+	     System.out.print("Check-out date (dd/MM/yyyy): ");
 	     Date checkOut = sdf.parse(sc.next());
 	     
 	     if(!checkOut.after(checkIn)) {
@@ -30,28 +30,22 @@ public class Program {
 	    	 
 	    	 System.out.println();
 		     System.out.println("Enter data to update the reservation: ");
-		     System.out.println("Check-in date (dd/MM/yyyy): ");
+		     System.out.print("Check-in date (dd/MM/yyyy): ");
 		     checkIn = sdf.parse(sc.next());
-		     System.out.println("Check-out date (dd/MM/yyyy): ");
+		     System.out.print("Check-out date (dd/MM/yyyy): ");
 		     checkOut = sdf.parse(sc.next());
 		     
-		     Date now = new Date();
-		     if (checkIn.before(now) || checkOut.before(now)) {
-		    	 System.out.println("Error in reservation: Reservation dates for update must be future dates");
-		     }
-		     else if (!checkOut.after(checkIn)) {
-		    	 System.out.println("Error in reservation: Check-out date must be after check-in date");
-		     }
-		     else {
-		    	 reservation.updateDate(checkIn, checkOut);
-		    	 System.out.println("Reservation: " + reservation);
-		     }
 		     
-			 
-	    }
 		
+		   	 String error = reservation.updateDate(checkIn, checkOut);
+		   	 if(error != null) {
+		   		System.out.println("Error in reservation: " + error);
+		   	 }
+		   	 else {
+		   		 System.out.println("Reservation: " + reservation);
+		     } 
+	    }		
 	     sc.close();
-
 	}
 
 }
